ea45a20a7dc967c22e7463a09142df098a23cd8f
mendezjw <mendezj@allegheny.edu>
2019-11-10 16:33:36 -0500

    Bringing website up to date.

--- src/_site/assets/js/index.js
+++ src/_site/assets/js/index.js
@@ -1,16 +1,17 @@
 // TypeWriter Animation
 var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 25,
-    backSpeed: 25,
-    backDelay: 1000,
-    startDelay: 1000,
-    loop: true
-  });
+  stringsElement: '#typed-strings',
+  typeSpeed: 25,
+  backSpeed: 25,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true
+});
 
-// Navigation Menu
-$(document).ready(function(){
-  $('.menu').click(function(){
-    $('ul').toggleClass('active');
+// Navigation Animation
+$(document).ready(function() {
+  $('.menu-toggle').click(function() {
+    $('.menu-toggle').toggleClass('active')
+    $('nav').toggleClass('active')
   })
 })
