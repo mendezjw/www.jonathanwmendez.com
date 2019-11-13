9be7ee0da8fac70217ae0032828912601177ea13
mendezjw <mendezj@allegheny.edu>
2019-11-13 18:34:17 -0500

    Removes fontawesome and replaces some min files with CDN links instead. This commit also includes a new navigation bar by Luxbar and creates a new head.html file in the includes.

---
+++ src/assets/js/site.js
@@ -0,0 +1,9 @@
+// TypeWriter Animation
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 25,
+  backSpeed: 25,
+  backDelay: 1000,
+  startDelay: 1000,
+  loop: true
+});
